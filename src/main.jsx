6645b3638d00bf51e09b242c2eee3f8f41172047
David Chibueze Ndubuisi <davydocsurg@gmail.com>
2022-02-17 14:12:53 +0100

    add no match route, url params,

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
+import "bootstrap";
 
 const rootElement = document.getElementById("root");
 render(
@@ -10,8 +12,18 @@ render(
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
-      </Route>  
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
     </Routes>
   </BrowserRouter>,
   rootElement
