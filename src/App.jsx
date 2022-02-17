5dfa4411f73e1dff2399dd3034154a6c469a552e
David Chibueze Ndubuisi <davydocsurg@gmail.com>
2022-02-17 14:47:48 +0100

    add active links, search params

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,42 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="mx-auto">
-      <header className="mx-auto">
-        <h1 className="">Hello World!</h1>
-        <nav
-          style={{
-            borderBottom: "solid 1px",
-            paddingBottom: "1rem",
-          }}
-        >
-          <Link to="/invoices">Invoices</Link> |{" "}
-          <Link to="/expenses">Expenses</Link>
-        </nav>
-      </header>
-      <Outlet />
-    </div>
+    <>
+      <div className="mx-auto">
+        <header className="mx-auto">
+          <h1 className="">Hello World!</h1>
+          <nav
+            style={{
+              borderBottom: "solid 1px",
+              paddingBottom: "1rem",
+            }}
+          >
+            <Link
+              className={
+                ("text-decoration-none",
+                ({ isActive }) =>
+                  isActive ? "text-success font-weight-bold" : "text-primary")
+              }
+              to="/invoices"
+            >
+              Invoices{" "}
+            </Link>
+            |{" "}
+            <Link
+              className={
+                ("text-decoration-none",
+                ({ isActive }) =>
+                  isActive ? "text-success font-weight-bold" : "text-primary")
+              }
+              to="/expenses"
+            >
+              Expenses
+            </Link>
+          </nav>
+        </header>
+        <Outlet />
+      </div>
+    </>
   );
 }
 
